1be93e2f5be940c2be9957d0cf2beeee9d8664e0
otomakorihciok <otomakorihciok@gmail.com>
2019-03-02 23:21:16 +0900

    Update swift for TensorFlow

--- tfswift/Sources/tfswift/main.swift
+++ tfswift/Sources/tfswift/main.swift
@@ -1,31 +1,34 @@
 import TensorFlow
 
+let hiddenSize = 10
+
 struct Model: Layer {
-  var l1, l2: Dense<Float>
+  var layer1 = Dense<Float>(inputSize: 4, outputSize: hiddenSize, activation: relu)
+  var layer2 = Dense<Float>(inputSize: hiddenSize, outputSize: hiddenSize, activation: relu)
+  var layer3 = Dense<Float>(inputSize: hiddenSize, outputSize: 3, activation: relu)
 
-  init(hiddenSize: Int) {
-    l1 = Dense(inputSize: 2, outputSize: hiddenSize, activation: relu)
-    l2 = Dense(inputSize: hiddenSize, outputSize: 1, activation: relu)
-  }
-
-  @differentiable(wrt: (self, input))
-  func applied(to input: Tensor<Float>) -> Tensor<Float> {
-    let h1 = l1.applied(to: input)
-    return l2.applied(to: h1)
+  @differentiable
+  func applied(to input: Tensor<Float>, in context: Context) -> Tensor<Float> {
+    let l1 = layer1.applied(to: input, in: context)
+    let l2 = layer2.applied(to: l1, in: context)
+    return layer3.applied(to: l2, in: context)
   }
 }
 
 let optimizer = SGD<Model, Float>(learningRate: 0.02)
-var classifier = Model(hiddenSize: 4)
-let x: Tensor<Float> = Tensor([[1, 2], [3, 4], [5, 6], [7, 8]])
-let y: Tensor<Float> = Tensor([0.1, 0.2, 0.3, 0.4])
+var classifier = Model()
+let context = Context(learningPhase: .training)
 
-for _ in 0 ..< 1000 {
-  let model = classifier.gradient { classifier -> Tensor<Float> in
-    let y_ = classifier.applied(to: x)
-    let loss = meanSquaredError(predicted: y_, expected: y)
+let x: Tensor<Float> = Tensor<Float>([[0.1, 0.2, 0.1, 0.3], [1.3, 2.1, 0.5, 3.9]])
+let y: Tensor<Float> = Tensor<Float>([[1, 2, 1], [3, 1, 5]])
+
+for _ in 0..<1000 {
+  let 𝛁model = classifier.gradient { classifier -> Tensor<Float> in
+    let ŷ = classifier.applied(to: x, in: context)
+    let loss = softmaxCrossEntropy(logits: ŷ, labels: y)
     print("Loss: \(loss)")
     return loss
   }
-  optimizer.update(&classifier, along: model)
+
+  optimizer.update(&classifier.allDifferentiableVariables, along: 𝛁model)
 }
